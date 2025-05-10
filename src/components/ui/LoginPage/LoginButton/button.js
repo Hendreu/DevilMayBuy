fc091f59c115229c9c3154b08e81fcd57d68ecb2
HendreuSellera <hendreu.tomadoce@sellera.ai>
2025-05-10 00:51:43 -0300

    prototype before react

---
+++ src/components/ui/LoginPage/LoginButton/button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './button.css';
+
+const LoginDefaultButton = ({children}) => {
+    
+    return(
+        <button className='LoginDefaultButton'>{children}</button>
+    )
+
+};
+
+export default LoginDefaultButton;
